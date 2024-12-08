587c1cb233052aae535f7de0fae26d0a1ddecbcd
naomiyjchen <naomichen.yj@gmail.com>
2024-12-08 11:06:37 +0000

    Remove files

--- run_spark.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ~/spark-project/.venv/bin/activate
-export PYSPARK_PYTHON=~/spark-project/.venv/bin/python
-export PYSPARK_DRIVER_PYTHON=~/spark-project/.venv/bin/python
-pyspark --deploy-mode client
